25d1e6c648935cc6d569e64b023aa5b8e5d0213f
Alexander Baltsevich <abaltsevich@klika-tech.com>
2014-12-28 15:58:44 +0300

    added fos user bundle

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -23,6 +23,8 @@ class AppKernel extends Kernel
             new Sonata\jQueryBundle\SonatajQueryBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
